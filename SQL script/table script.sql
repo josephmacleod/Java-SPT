3646dd65c3588d3a8d880584cb1e5d4c9aa669f3
josephmacleod <13000146@uhi.ac.uk>
2017-01-16 11:43:33 +0000

    Update table script.sql

--- SQL script/table script.sql
+++ SQL script/table script.sql
@@ -1,7 +1,7 @@
 CREATE TABLE spt(
-	date_time       DATETIME       NOT NULL,
-    Hs				NUMERIC(8,3)   NOT NULL,
-    Tz				NUMERIC(6,3)   NOT NULL,
-    Battery         NUMERIC(2)     NOT NULL,
-	PRIMARY KEY (date_time)
+date_time DATETIME NOT NULL,
+Hs NUMERIC(8,3) NOT NULL,
+Tz NUMERIC(6,3) NOT NULL,
+Battery NUMERIC(2) NOT NULL,
+PRIMARY KEY (date_time)
 );
