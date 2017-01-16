73caab1a04819892a08dfa3f178907101c80c801
josephmacleod <13000146@uhi.ac.uk>
2017-01-16 11:55:21 +0000

    String filename added
    datetime and String filename added

--- readdata/DataSPT.java
+++ readdata/DataSPT.java
@@ -3,16 +3,20 @@ package readdata;
 
 public class DataSPT{
 
-  Double Hs = null;
-  Double Tz = null;
-  Double Battery = null;
+  Double hs = null;
+  Double tz = null;
+  Double battery = null;
+  String path = null;
+  String datetime = null;
   
-  public DataSPT(String[] file_data){
+  public DataSPT(String[] file_data, String path){
     
-    Hs = new Double(file_data[1]);
-    Tz = new Double(file_data[2]);
-    Battery = new Double(file_data[6]);
-  
+    hs = new Double(file_data[1]);
+    tz = new Double(file_data[2]);
+    battery = new Double(file_data[6]);
     
+    String filename = path.substring(path.lastIndexOf("\\") + 1);
+    
+    datetime = filename.substring(0,filename.length()-4);
   }
 }
