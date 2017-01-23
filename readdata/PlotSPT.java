daf03bcdf298912a869b4dbb75e12645fa30587a
josephmacleod <13000146@uhi.ac.uk>
2017-01-23 13:04:14 +0000

    Created PlotSPT.java
    PlotSPT has no main class

---
+++ readdata/PlotSPT.java
@@ -0,0 +1,25 @@
+package readdata;
+
+import java.io.*;
+import java.util.*;
+import org.jfree.chart.JFreeChart; 
+import org.jfree.chart.ChartFactory; 
+import org.jfree.chart.ChartUtilities; 
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class PlotSPT{
+  
+  public Hashtable<String,Hashtable> hashrecords;
+  
+  public PlotSPT(Hashtable<String,Hashtable> records){
+    hashrecords = records;
+  }
+  
+  public void getHashtable(ReadDB readDB){
+    
+  }
+  
+
+    
+}
