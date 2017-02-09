0e91c6454144c037312db18d3c61fba3dff38ed6
josephmacleod <13000146@uhi.ac.uk>
2017-02-09 13:48:22 +0000

    Created ReadSPT.java
    Changed package to name 'process_spt'.

---
+++ process_spt/ReadSPT.java
@@ -0,0 +1,34 @@
+package process_spt;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Arrays;
+
+public class ReadSPT{
+  
+  private String path;
+  
+  public ReadSPT(String file_path){
+    path = file_path;
+  }
+
+  public String[] OpenFile() throws IOException{
+    FileReader fr = new FileReader(path);
+    BufferedReader textReader = new BufferedReader(fr);
+    
+    int i;
+    
+    int textlisting = 12;
+    String[] lines = new String[textlisting];
+            
+       for (i=0; i < lines.length; i++) {
+          lines[i] = textReader.readLine();
+          
+       } 
+    System.out.println(Arrays.toString(lines));
+    
+    return lines;
+    }
+    
+  }
