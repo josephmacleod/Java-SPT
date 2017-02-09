da1920370591dd70022ea74ec1ed91d66fc10e0b
josephmacleod <13000146@uhi.ac.uk>
2017-02-09 13:46:42 +0000

    Created IterateSPT
    Changed package to name 'process_spt'.

---
+++ process_spt/IterateSPT.java
@@ -0,0 +1,30 @@
+package process_spt;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.*;
+
+public class IterateSPT {
+  
+  public static void main(String[] args) throws IOException {
+    String directory_path = "E:\\Documents\\Object Oriented Programming\\James Morrison\\Example file read\\spt_files";
+    
+    File folder = new File(directory_path);
+    File[] listOfFiles = folder.listFiles();
+   
+    StoreSPT storeSPT = new StoreSPT();
+    
+    for (File file : listOfFiles) {
+      if (file.isFile()) {
+        ReadSPT spt_data = new ReadSPT(file.getPath());
+        String[] file_data = spt_data.OpenFile();
+        DataSPT dataSPT = new DataSPT(file_data, file.getPath());
+        storeSPT.writeRecord(dataSPT);
+        
+      } 
+      
+    };
+    
+  }
+}
