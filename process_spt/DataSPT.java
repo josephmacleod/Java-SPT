1d65e2615acd162e8a135bc09cad13108a9ab667
josephmacleod <13000146@uhi.ac.uk>
2017-02-09 13:45:41 +0000

    Created DataSPT.java
    Changed package to name 'process_spt'.

---
+++ process_spt/DataSPT.java
@@ -0,0 +1,21 @@
+package process_spt;
+
+public class DataSPT{
+
+   Double hs = null;
+   Double tz = null;
+   Double battery = null;
+   String path = null;
+   String datetime = null;
+   
+   public DataSPT(String[] file_data, String path){
+     
+     hs = new Double(file_data[1]);
+     tz = new Double(file_data[2]);
+     battery = new Double(file_data[6]);
+     
+     String filename = path.substring(path.lastIndexOf("\\") + 1);
+     
+     datetime = filename.substring(0,filename.length()-4);
+   }
+ }
